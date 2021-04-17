fff3d1e01763060099324837c95bce1abf184082
Misiar <misiar9@interia.pl>
2021-04-17 21:47:07 +0200

    LoginForm (zczytuje dane)

--- react-od-podstaw-pusty/src/App.js
+++ react-od-podstaw-pusty/src/App.js
@@ -1,16 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import LoginForm from './component/LoginForm';
 
 function App() {
-  const adminUser= {
+  const adminUser = {
     name: "Misiar",
     password: "admin"
   }
- 
- 
- 
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+  }
+  const Logout = () => {
+    console.log("Logout");
+
+  }
+
   return (
     <div className="App">
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button>Logout</button>
+        </div>
+      ) : (<LoginForm Login={Login} error={error} />)}
     </div>
   );
 }
