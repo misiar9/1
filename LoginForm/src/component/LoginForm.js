23ae13d35d74d84c9ac5351f7f01eacdc40b1fe5
Misiar <misiar9@interia.pl>
2021-04-20 21:49:28 +0200

    Css

--- LoginForm/src/component/LoginForm.js
+++ LoginForm/src/component/LoginForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import './LoginForm.css';
+import tipico from './img/tipico.jpg';
 
 function LoginForm({ Login, error }) {
     const [details, setDetails] = useState({ name: "", password: "" });
@@ -10,17 +12,16 @@ function LoginForm({ Login, error }) {
     return (
         <form onSubmit={submitHandler}>
             <div className="form-inner">
-                <h2>Login</h2>
+                <h3>Tipico Retail Franchisee Portal</h3>
+                <img src={tipico} width="500px"></img>
                 {/* ERROR! */}
                 <div className="form-group">
-                    <label htmlFor="name">Name:</label>
                     <input type="text" name="name" id="name" onChange={e => setDetails({ ...details, name: e.target.value })} value={details.name} />
                 </div>
                 <div className="form-group">
-                    <label htmlFor="passowrd">Passowrd:</label>
                     <input type="password" name="password" id="password" onChange={e => setDetails({ ...details, password: e.target.value })} value={details.password} />
                 </div>
-                <input type="submit" value="Login" />
+                <input type="submit" id="submit" value="Login" />
             </div>
         </form>
     )
